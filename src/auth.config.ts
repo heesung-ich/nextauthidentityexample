71864215457a873ae037fcd74cd236915bc6b7af
Roslyn <roslyn@icloudhospital.com>
2024-03-13 14:25:10 +0900

    first commit

---
+++ src/auth.config.ts
@@ -0,0 +1,19 @@
+import type { NextAuthConfig } from "next-auth";
+
+export const authConfig = {
+    callbacks: {
+        signIn({ user, account, profile, email, credentials }) {
+            return true;
+        },
+        redirect({ url, baseUrl }) {
+            return baseUrl;
+        },
+        session({ session, user, token }) {
+            return session;
+        },
+        jwt({ token, user, account, profile, isNewUser }) {
+            return token;
+        }
+    },
+    providers: [],
+} satisfies NextAuthConfig;
---
+++ src/auth.ts
@@ -0,0 +1,21 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6"
+import { authConfig } from './auth.config';
+ 
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        console.log('credentials : ', credentials);
+        return null;
+      },
+    }),
+    DuendeIDS6Provider({
+        clientId: "",
+        clientSecret: "",
+        issuer: "",
+    })
+  ],
+});
